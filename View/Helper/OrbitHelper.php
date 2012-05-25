22b43a5443fc26475b5fe2b40b9c7604317e8060
git <arie.xintesa@gmail.com>
2012-05-25 17:33:38 +0700

    modify orbit helper

--- View/Helper/OrbitHelper.php
+++ View/Helper/OrbitHelper.php
@@ -12,21 +12,29 @@ class OrbitHelper extends AppHelper {
 	}
 
 	function album($album, $photos) {
-		return $this->Html->tag('div', $photos, array(
-			'id' => 'gallery-' . $album['Album']['id'],
+		$galleryId = 'gallery' . Inflector::camelize($album['Album']['slug']);
+		$result = $this->Html->tag('div', $photos, array(
+			'id' => $galleryId,
 			));
+		return $result;
 	}
 
 	function photo($album, $photo) {
 		$urlLarge = $this->Html->url('/' . $photo['large']);
 		$urlSmall = $this->Html->url('/' . $photo['small']);
-		return $this->Html->image($urlLarge);
+		$urlOriginal = $this->Html->url('/' . $photo['original']);
+		$result = $this->Html->image($urlOriginal);
+		return $result;
 	}
 
 	function initialize($album) {
-		$config = $this->Gallery->getAlbumJsParams($album);
+		//$config = $this->Gallery->getAlbumJsParams($album);
+		$config = '{bullets: true, directionalNav: false, timer: false}';
+		$param = str_replace('"', '', $config);
+		$galleryId = 'gallery' . Inflector::camelize($album['Album']['slug']);
+
 		$js = sprintf('$(\'#%s\').orbit(%s);',
-			'gallery-' . $album['Album']['id'],
+			$galleryId,
 			$config
 			);
 		$this->Js->buffer($js);
